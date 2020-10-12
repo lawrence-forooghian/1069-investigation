904e1a6852a422e18189dc344f90183e6cb8651e
Lawrence Forooghian <lawrence@forooghian.com>
2020-10-12 22:15:25 +0100

    Initial Commit

---
+++ Ably1069/Ably1069App.swift
@@ -0,0 +1,17 @@
+//
+//  Ably1069App.swift
+//  Ably1069
+//
+//  Created by Lawrence Forooghian on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Ably1069App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ably1069/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ably1069
+//
+//  Created by Lawrence Forooghian on 12/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
